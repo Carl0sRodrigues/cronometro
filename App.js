5633b7b4653a3ae6d3d3baa5fef82e3e4135c9ce
Carl0sRodrigues <carlos_._rodrigues@outlook.com>
2021-11-15 13:19:23 -0300

    Folha de estilo pronto

--- App.js
+++ App.js
@@ -6,16 +6,20 @@ export default function App(){
   <View style={styles.conteiner}>
     <Image
     source={require('./assets/src/crono.png')}
+    style={{marginBottom: 20}}
     />
-    <Text> 00:00:00 </Text>
-    <View>
-      <TouchableOpacity>
-        <Text> INICIAR </Text>
+    <Text style={styles.timer}> 00:00:00 </Text>
+    <View style={[styles.area, {padding: 10}]}>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.btntexto}> INICIAR </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Text> ZERAR </Text>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.btntexto}> ZERAR </Text>
       </TouchableOpacity>
+    </View>
+    <View style={styles.areatempo}>
+      <Text style={styles.tempo}>Ultimo Tempo</Text>
     </View>
   </View>
   );
@@ -27,5 +31,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#00aeef'
+  },
+  timer:{
+    marginTop: -190,
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 45
+  },
+  botao:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 17,
+    height: 35,
+    backgroundColor: '#fff',
+    borderRadius: 10
+  },
+  area:{
+    marginTop: 130,
+    flexDirection: 'row'
+  },
+  btntexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areatempo:{
+    
+  },
+  tempo:{
+
   }
 })
